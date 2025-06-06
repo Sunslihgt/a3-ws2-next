77902f252af03ec0f7ac60ca0eab0698f96f70f4
Sunslihgt <samsam.deschamps@gmail.com>
2025-06-06 11:04:58 +0200

    Added dummy cart page

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/types/Product";
+import { useAppDispatch } from '@/store/hooks';
 
 type ProductCardProps = {
     product: Product;
@@ -8,6 +9,7 @@ export default function ProductCard({ product }: ProductCardProps) {
     return (
         <div className="bg-white rounded-lg shadow-md p-6 flex flex-col items-center hover:shadow-lg transition-shadow">
             <img src={product.image} alt={product.title} className="w-32 h-32 object-contain mb-4" />
+
             <h2 className="text-lg font-semibold text-gray-800 mb-2 text-center">
                 {product.title}
             </h2>
